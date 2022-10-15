a1c8556677b4f8e546adcd096bf89bc8b7b87136
elmerdu69 <115868585+elmerdu69@users.noreply.github.com>
2022-10-15 21:41:53 +0200

    Update configuration.php
    mise en forme de la page

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -28,28 +28,8 @@ if (!isConnect()) {
       <label class="col-md-4 control-label">{{Heure de mise à jour}}
         <sup><i class="fas fa-question-circle tooltips" title="{{Heure à laquelle le plugin va chercher les informations}}"></i></sup>
       </label>
-      <div class="col-md-4">
+      <div class="col-md-1">
         <input class="configKey form-control" data-l1key="cronHeure"/>
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="col-md-4 control-label">{{Global param 2}}
-        <sup><i class="fas fa-question-circle tooltips" title="{{Renseignez le paramètre 2 du plugin}}"></i></sup>
-      </label>
-      <div class="col-md-4">
-        <input class="configKey form-control" data-l1key="param2"/>
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="col-md-4 control-label">{{Global param 3}}
-        <sup><i class="fas fa-question-circle tooltips" title="{{Sélectionnez du paramètre 3 du plugin}}"></i></sup>
-      </label>
-      <div class="col-md-4">
-        <select class="configKey form-control" data-l1key="param3">
-          <option value=""></option>
-          <option value="value1">value1</option>
-          <option value="value2">value2</option>
-        </select>
       </div>
     </div>
   </fieldset>
